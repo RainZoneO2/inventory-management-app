00b0695bafbe96cb0b6189aea9a2214c1f5f24de
Ghaith A <alghaith.ah38@gmail.com>
2024-08-06 01:22:18 -0400

    Temporary lock to force users to auth on any page past landing page.

--- middleware.js
+++ middleware.js
@@ -1,11 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/landing(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/trial(.*)", "/pantry(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  // if (isProtectedRoute(req)) {
-  //   auth().protect();
-  // }
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
